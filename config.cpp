484d11f48ff620b5871a858315c4e6acf1c12047
kenoxite <kenoxite@gmail.com>
2025-05-02 20:39:20 +0200

    Update config.cpp

--- config.cpp
+++ config.cpp
@@ -5,7 +5,7 @@ class CfgPatches
         name = "Squad Feedback";
         author = "kenoxite";
         authors[] = {"kenoxite"};
-        version = "3.2.1";
+        version = "3.2.2";
         //url = "";
 
         requiredVersion = 2.14; 
